fd324b8b94d303900dd6d0c9c9d1b6a80e5bea57
rayvenrey <rayven.rey@gmail.com>
2021-11-10 20:14:35 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      # /restaurants/top
+      get :index
+    end
+
+    member do
+      # /restaurants/:id/chef
+    end
+    resources :reviews, only: [:new, :create]
+  end
 end
